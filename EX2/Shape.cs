cea2d64de1a4b179a25017c0d548df72636bc758
Novel Poudel <npoudel21@my.whitworth.edu>
2020-01-13 14:39:08 -0800

    started EX2

---
+++ EX2/Shape.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1
+{
+    class Shape //: IComparable
+    {
+        public string Color
+        {
+            get;
+            set;
+        }
+        public Shape(string c = "") { Color = c; }
+        public string getColor() { return Color; }
+        public void setColor(string c) { Color = c; }
+        public virtual double getPerimeter() { return 0; }
+
+        public virtual double getArea() { return 0; }
+
+        //public int CompareTo(object o)
+        //{
+        //    if (o == null)
+        //    {
+        //        return -1;
+        //    }
+        //    Shape newObject = o as Shape;
+        //    if (newObject != null)
+        //    {
+        //        return getArea().CompareTo(newObject.getArea());
+        //    }
+        //    else
+        //    {
+        //        throw new ArgumentException("Object is not a Shape");
+        //    }
+        //}
+    }
+
+    class Circle : Shape
+    {
+        int radius;
+        public Circle(int r = 0, string c = "") : base(c) { radius = r; }
+        public int getRadius() { return radius; }
+        public void setRadius(int r) { radius = r; }
+        public override double getPerimeter()
+        {
+            return 2 * 3.1415 * radius;
+        }
+
+        public override double getArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    //I set EqPolygon to inherit IComparable so that the I get an error when I try to compare with Circle
+    class EqPolygon : Shape, IComparable
+    {
+        protected int sides;
+
+        public int SideLen
+        {
+            get;
+            set;
+        }
+
+        public EqPolygon(int len = 0, string c = "", int s = 3) : base(c)
+        {
+            SideLen = len;
+            sides = s;
+        }
+
+        public int getSides() { return sides; }
+
+        public int CompareTo(object o)
+        {
+            if (o == null)
+                return 1;
+            EqPolygon newPoly = o as EqPolygon;
+            if (newPoly == null)
+                throw new ArgumentException("Object is not an EqPolygon");
+            else
+            {
+                return sides.CompareTo(newPoly.sides);
+            }
+        }
+    }
+
+    class Triangle : EqPolygon
+    {
+        public Triangle(int len = 0, string c = "") : base(len, c,3) { }
+        public override double getPerimeter() { return sides * SideLen; }
+
+        public override double getArea()
+        {
+            return Math.Pow(3, 0.5) * SideLen * SideLen / 4;
+        }
+    }
+
+    class Square : EqPolygon
+    {
+        public Square(int len = 0, string c = "") : base(len, c,4) { }
+        public override double getPerimeter() { return 4 * SideLen; }
+
+        public override double getArea()
+        {
+            return SideLen * SideLen;
+        }
+    }
+
+    class Pentagon : EqPolygon
+    {
+        public Pentagon(int len = 0, string c = "") : base(len, c,5) { }
+        public override double getPerimeter() { return 5 * SideLen; }
+
+        public override double getArea()
+        {
+            return Math.Pow(5 * (5 + 2 * Math.Pow(5, 0.5)), 0.5) * SideLen * SideLen / 4;
+        }
+    }
+}
---
+++ EX2/Shapes.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EX1;
+
+namespace EX2
+{
+    //enumerable and enumerator classes for Shapes
+    class Shapes : IEnumerable
+    {
+        public Shape[] ShapeArray
+        {
+            get;
+            set;
+        }
+
+        public Shapes(Shape[] s)
+        {
+            ShapeArray = new Shape[s.Length];
+            for (int i = 0; i < ShapeArray.Length; ++i)
+            {
+                ShapeArray[i] = s[i];
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new ShapeEnumerator(ShapeArray);
+        }
+
+    }
+
+    class ShapeEnumerator : IEnumerator
+    {
+        public Shape[] ShapeArray
+        {
+            get;
+            set;
+        }
+        private int position = -1;
+
+        public ShapeEnumerator(Shape[] s)
+        {
+            ShapeArray = s;
+
+        }
+
+        public object Current
+        {
+            get
+            {
+                try
+                {
+                    return ShapeArray[position];
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        public bool MoveNext()
+        {
+            ++position;
+            return position < ShapeArray.Length;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+
+
+    //enumerable and enumerator classes for ComparableShapes
+    class ComparableShapes
+    {
+        
+    }
+}
